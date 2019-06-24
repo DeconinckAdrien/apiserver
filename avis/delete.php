3def4fc13e83678d4bcf735789d65615b48ac667
DeconinckAdrien <50237096+DeconinckAdrien@users.noreply.github.com>
2019-06-24 10:57:10 +0200

    Update delete.php

--- avis/delete.php
+++ avis/delete.php
@@ -10,36 +10,36 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 include_once '../objects/avis.php';
 
-// get database connection
+// connection à la base
 $database = new Database();
 $db = $database->getConnection();
 
-// prepare avis object
+// instanciation de l'objet avis
 $avis = new Avis($db);
 
-// get avis id
+// on récupère l'ID de l'avis à supprimer 
 $data = json_decode(file_get_contents("php://input"));
 
-// set avis id to be deleted
+// modification de la propriété d'objet ID 
 $avis->avis_id = $data->id;
 
-// delete the avis
+// Suppression de l'avis 
 if($avis->delete()){
 
     // set response code - 200 ok
     http_response_code(200);
 
-    // tell the user
+    // Envoie du message de confirmation
     echo json_encode(array("message" => "Événement supprimé"));
 }
 
-// if unable to delete the avis
+// Si le message n'a pas pu être supprimer 
 else{
 
     // set response code - 503 service unavailable
     http_response_code(503);
 
-    // tell the user
+    // Envoie du message d'erreur
     echo json_encode(array("message" => "Impossible de supprimer l'évènement"));
 }
 ?>
