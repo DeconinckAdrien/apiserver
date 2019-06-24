122b68487729221823bfa1d2c93607914daf0556
DeconinckAdrien <50237096+DeconinckAdrien@users.noreply.github.com>
2019-06-24 10:59:38 +0200

    Update read.php

--- avis/read.php
+++ avis/read.php
@@ -1,26 +1,27 @@
 <?php
-// required headers
+//  headers necessaire
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// include database and object files
 include_once '../config/database.php';
 include_once '../objects/avis.php';
 
-// instantiate database and avis object
+// instanciation de l'objet Database
 $database = new Database();
+// connection à la base de données
 $db = $database->getConnection();
 
-// initialize object
+// instanciation de l'objet avis
 $avis = new Avis($db);
 
-// query avis
+// lecture de l'avis 
 $stmt = $avis->read();
+// récupère la ligne 
      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $avis_arr["records"]=$result;
 
     // set response code - 200 OK
     http_response_code(200);
 
-    // show aviss data in json format
+    // envoie l'avis au format Json
     echo json_encode($avis_arr);
