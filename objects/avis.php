e6188d676da73e096103b4db00f70e1ec0b05bc2
DeconinckAdrien <50237096+DeconinckAdrien@users.noreply.github.com>
2019-06-24 10:26:24 +0200

    Update avis.php

--- objects/avis.php
+++ objects/avis.php
@@ -5,39 +5,40 @@ class Avis{
     private $conn;
 
 
-    // object properties
+    // propriétés d'objet
     public $name;
     public $description;
     public $created;
-    public $avis_id;
+    public $avis_id; // identificateur 
     public $modified;
     public $desc='description';
 
-    // constructor with $db as database connection
+    // constructeur de l'objet avis 
     public function __construct($db){
         $this->conn = $db;
     }
 
-    // read products
+    // Fonction de lecture d'un avis 
     function read(){
-
+        // envoie d'une requêtte SQL et récupération de sa réponse
          $stmt = $this->conn->prepare("SELECT * FROM avis");
          $stmt->execute();
 
         return $stmt;
     }
-    // create product
+    // Création d'un avis 
 
     function create(){
           $this->name=htmlspecialchars(strip_tags($this->name));
           $this->description=htmlspecialchars(strip_tags($this->description));
           $this->created=htmlspecialchars(strip_tags($this->created));
-
+        
       $data = [
           'name' => $this->name,
           $this->desc => $this->description,
           'created' => $this->created,
       ];
+        // requêtte SQl 
       $sql = "INSERT INTO avis (name, description, created) VALUES (:name, :description, :created)";
       $stmt=  $this->conn->prepare($sql);
       $executed = $stmt->execute($data);
@@ -52,22 +53,23 @@ class Avis{
     // used when filling up the update product form
 function readOne(){
 
-  // sanitize
+ // récupération des propriétés de l'objet pour un update
   $this->name=htmlspecialchars(strip_tags($this->name));
   $this->description=htmlspecialchars(strip_tags($this->description));
   $this->avis_id=htmlspecialchars(strip_tags($this->avis_id));
   $this->created=htmlspecialchars(strip_tags($this->created));
 
-
+    
   $stmt = $this->conn->prepare("SELECT * FROM avis where avis_id=:id");
+  // envoie requêtte SQL
   $stmt->execute(array('id' => $this->avis_id));
 
-    // get retrieved row
+    // Obtient la ligne récupérée
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
 
-    // set values to object properties
+   // Modifie les valeurs des propriétés de l'objet
     $this->name = $row['name'];
     $this->description = $row['description'];
     $this->avis_id = $row['avis_id'];
@@ -76,18 +78,20 @@ function readOne(){
 
 }
 
+    // lecture du dernier avis publier
 function readLast(){
 
-
+  
   $stmt = $this->conn->prepare("SELECT * FROM avis where avis_id in (SELECT MAX(avis_id) from avis)");
+  // envoie requêtte SQL select
   $stmt->execute();
 
-    // get retrieved row
+    // Obtient la ligne récupérée
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
 
-    // set values to object properties
+    // Modifie les valeurs des propriétés de l'objet
     //$desc='description';
     $this->name = $row['name'];
     $this->description = $row[$this->desc];
@@ -97,12 +101,13 @@ function readLast(){
 
 }
 
-// update the product
+// Met à jour un avis
 function update(){
 
 
-    // update query
+    
     $stmt = $this->conn->prepare("UPDATE avis SET name = :name, description = :description WHERE avis_id = :id");
+    // Envoie requêtte SQL 
     $exec=$stmt->execute(array(
       'id' => $this->avis_id,
       $this->desc => $this->description,
@@ -111,7 +116,7 @@ function update(){
 
     $count = $stmt->rowCount();
 
-
+    //
     if($count==1){
         return true;
     }
@@ -125,12 +130,12 @@ function update(){
     // return false;
 }
 
-// delete the product
+// supprime un avis 
 function delete(){
 
-    // delete query
+    
     $stmt = $this->conn->prepare("DELETE FROM avis WHERE avis_id = :id");
-
+    // supprime dans la base de donnée 
     $stmt->execute(array(
       'id' => $this->avis_id,
     ));
@@ -146,7 +151,7 @@ function delete(){
 
 }
 
-// search products
+// Fonction de recherche d'un avis 
 function search($keywords){
 $keywords = "%{$keywords}%";
     // select all query
