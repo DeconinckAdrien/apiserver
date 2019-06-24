eecf3bb077d1be55f44aa4ef7c2869ac6e5c3144
DeconinckAdrien <50237096+DeconinckAdrien@users.noreply.github.com>
2019-06-24 10:47:32 +0200

    Update create.php
    comment in french

--- avis/create.php
+++ avis/create.php
@@ -6,57 +6,57 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// get database connection
-include_once '../config/database.php';
 
-// instantiate avis object
+include_once '../config/database.php';
 include_once '../objects/avis.php';
 
 $database = new Database();
+// connection à la base de données 
 $db = $database->getConnection();
 $msg="message";
+// instanciation de l'objet avis 
 $avis = new Avis($db);
 
-// get posted data
+// récupère les données du formulaire et vérification de leurs contenus
 $data = json_decode(file_get_contents("php://input"));// make sure data is not empty
 if(
     !empty($data->name) &&
     !empty($data->description)
 ){
 
-    // set avis property values
+    // change les valeurs des propriétés de l'objet 
     $avis->name = $data->name;
     $avis->description = $data->description;
     $avis->created = date('Y-m-d H:i:s');
-
-    // create the avis
+   
+    // appelle de la fonction créé avis 
     if($avis->create()){
 
         // set response code - 201 created
         http_response_code(201);
 
-        // tell the user
+        // Envoie du message de confirmation de la création de l'avis 
         echo json_encode(array($msg => "Événement partagé"));
     }
 
-    // if unable to create the avis, tell the user
+    // Si cela n'a pas fonctionné 
     else{
 
         // set response code - 503 service unavailable
         http_response_code(503);
 
-        // tell the user
+        // Envoie du message d'echec
         echo json_encode(array($msg => "Erreur lors du partage de l'événement"));
     }
 }
 
-// tell the user data is incomplete
+// données incomplètes
 else{
 
     // set response code - 400 bad request
     http_response_code(400);
 
-    // tell the user
+    // envoie du message d'erreur
     echo json_encode(array($msg => "Erreur lors du partage de l'événement. Données incomplètes."));
 }
 ?>
